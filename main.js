67e18ffff8ee270c53ff07f0690bd916eea74e5f
Chris Nelson <chrisnelson7791@gmail.com>
2023-09-28 16:18:53 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function numberToString(num) {
+  return num.toString()
+}
+
+console.log(numberToString(456))
+
+// parameter: number
+// return: number converted to a string
+// e.g. numberToString(456)
+// results in '456'
+
+// return num.toString()
